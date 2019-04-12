3e53320305fcfaeaa678014f195c2172a06d4eb2
unknown <stalin>
2019-04-12 11:02:55 +0530

    Added customized theme

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
